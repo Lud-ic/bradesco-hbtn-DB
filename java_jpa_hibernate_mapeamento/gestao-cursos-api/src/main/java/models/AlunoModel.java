8105c9c5c72b68c88bf479bb357e6bde37e88c5c
Ludmila Magalhães <mag.ludmila@gmail.com>
2025-10-15 23:31:11 -0300

    add AlunoModel

--- java_jpa_hibernate_mapeamento/gestao-cursos-api/src/main/java/models/AlunoModel.java
+++ java_jpa_hibernate_mapeamento/gestao-cursos-api/src/main/java/models/AlunoModel.java
@@ -28,20 +28,72 @@ public class AlunoModel {
     }
 
     public Aluno findById(Long id) {
-        // TODO
-        return null;
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("gestao-cursos-jpa");
+        EntityManager em = emf.createEntityManager();
+        Aluno aluno = null;
+        try {
+            aluno = em.find(Aluno.class, id);
+        } catch (Exception e) {
+            em.close();
+            System.err.println("Erro ao buscar aluno por ID: " + e.getMessage());
+        } finally {
+            em.close();
+        }
+        return aluno;
     }
 
     public List<Aluno> findAll() {
-        // TODO
-        return null;
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("gestao-cursos-jpa");
+        EntityManager em = emf.createEntityManager();
+        List<Aluno> alunos = null;
+        try {
+            alunos = em.createQuery("SELECT a FROM Aluno a", Aluno.class).getResultList();
+        } catch (Exception e) {
+            em.close();
+            System.err.println("Erro ao buscar todos os alunos: " + e.getMessage());
+        } finally {
+            em.close();
+        }
+        return alunos;
     }
 
     public void update(Aluno aluno) {
-        // TODO
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("gestao-cursos-jpa");
+        EntityManager em = emf.createEntityManager();
+        try {
+            em.getTransaction().begin();
+            em.merge(aluno);
+            em.getTransaction().commit();
+            System.out.println("Aluno atualizado com sucesso !!!");
+        } catch (Exception e) {
+            em.close();
+            System.err.println("Erro ao atualizar o aluno: " + e.getMessage());
+        } finally {
+            em.close();
+            System.out.println("Finalizando a transação");
+        }
     }
 
     public void delete(Aluno aluno) {
-        // TODO
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("gestao-cursos-jpa");
+        EntityManager em = emf.createEntityManager();
+        try {
+            em.getTransaction().begin();
+            Aluno managedAluno = em.find(Aluno.class, aluno.getId());
+            if (managedAluno != null) {
+                em.remove(managedAluno);
+                em.getTransaction().commit();
+                System.out.println("Aluno removido com sucesso !!!");
+            } else {
+                System.out.println("Aluno não encontrado para remoção.");
+                em.getTransaction().rollback();
+            }
+        } catch (Exception e) {
+            System.err.println("Erro ao remover o aluno: " + e.getMessage());
+            em.getTransaction().rollback();
+        } finally {
+            em.close();
+            System.out.println("Finalizando a transação");
+        }
     }
 }
